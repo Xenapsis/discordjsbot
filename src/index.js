125e49f9cfbf22016ff64a6ab395d55bcaecf836
Xenapsis <149744857+Xenapsis@users.noreply.github.com>
2024-02-25 13:48:05 -0500

    slight changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
-const {Client, IntentsBitField, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, ComponentType, ButtonInteraction, ButtonComponent, InteractionCollector, InteractionReplyOptions,} = require('discord.js')
+const {Client, Events, GatewayIntentBits, IntentsBitField, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle, ComponentType, ButtonInteraction, ButtonComponent, InteractionCollector, InteractionReplyOptions,} = require('discord.js')
 const mongoose = require('mongoose');
-const mongoURL = process.env.mongoURL;
+const sqlite = require('sqlite3').verbose();
 
 const buttonCooldown = new Set()
 const buttonCooldown2 = new Set()
-
+var query = `SELECT * FROM data WHERE userid = ?`;
+var total = null;
+let newrow = null;
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 
 const client = new Client({
     intents: [
@@ -17,30 +22,25 @@ const client = new Client({
 });
 
 
-
 client.on('ready', (c) => {
     console.log(`${c.user.tag} Is online`)
-    async () => {
-        try {
-            mongoose.set('strictQuery', false)
-            await mongoose.connect(mongoURL, { keepAlive: true });
-            console.log("connected to mongoose")
-            eventHandler(client);
-        } catch (error) {
-            console.log(`Could not connect because of ${error}`)
-        }
-    };
+    let db = new sqlite.Database('./utils/database/database.db', sqlite.OPEN_READWRITE | sqlite.OPEN_CREATE);
 })
 
-client.on('interactionCreate',(interaction) => {
+client.on('message', (message) => {
+    let msg = message.content.toLowerCase();
+    console.log(msg)
+});
 
-const links = ["https://Griniszaddy.jaluzinov.ru", "https://goofygoober.jaluzinov.ru/", "https://Ianeatsjunkfood.gerastar.ru", "https://lightspeedredneck.jaluzinov.ru"];
-const premium = ["https://freebsd.cnew.ir", "https://xenapsissolosgamebyte.sorynov.cl", "https://riplightspeed.cnew.ir", "https://getbypassed.jaluzinov.ru"]
+client.on('interactionCreate',(interaction) => {
+    let db = new sqlite.Database('./utils/database/database.db', sqlite.OPEN_READWRITE);
+    const links = ["https://Griniszaddy.jaluzinov.ru", "https://goofygoober.jaluzinov.ru/", "https://Ianeatsjunkfood.gerastar.ru", "https://lightspeedredneck.jaluzinov.ru"];
+    const premium = ["https://freebsd.cnew.ir", "https://xenapsissolosgamebyte.sorynov.cl", "https://riplightspeed.cnew.ir", "https://getbypassed.jaluzinov.ru"]
 
 
 
     if (interaction.commandName === 'links') {
-        if (!interaction.member.roles.cache.has(`1181807862360592434`)) {
+        if (!interaction.member.roles.cache.has(`1179905654345957488`)) {
             interaction.reply({content: 'Sorry but your not a helper bwomp',ephemeral: true})
             return;
         }
@@ -49,10 +49,11 @@ const premium = ["https://freebsd.cnew.ir", "https://xenapsissolosgamebyte.soryn
         .setTitle("Shady Link Dispenser")
         .setDescription("Please turn on your dms before using this bot or you might lose your free link")
         .setColor(0x5900FF)
-        .addFields({name: 'Free Users 🎫', value: '1 use per 1 week', inline: true})
-        .addFields({name: 'Premium Users (Boosters) 🔮', value: '1 use per 1 week + free uses', inline: true})
-        .addFields({name: 'Do NOT Share 🔨', value: 'Sharing will result in a ban womp'})
-        .addFields({name: 'Links Changed every sunday', value: 'Links change at 12am est on Sundays', inline: true})
+        .addFields({name: 'Free Users 🎫', value: '1 use per 1 day', inline: true})
+        .addFields({name: 'Premium Users (Boosters) 🔮', value: '1 use per 1 day + free uses', inline: true})
+        .addFields({name: 'Do NOT Share 🔨', value: 'Sharing will result in a ban and the website getting blocked faster by an admin'})
+        .addFields({name: 'Premium links are proven to work and updated more often', value: 'Free links may be outdated or blocked!'})
+        .addFields({name: 'Links Changed every Month', value: 'Links change at the first day of the month', inline: true})
         .setThumbnail('https://xenapsis.github.io/imgs/favicon.png')
 
         const button = new ButtonBuilder()
@@ -72,72 +73,174 @@ const premium = ["https://freebsd.cnew.ir", "https://xenapsissolosgamebyte.soryn
 
     }
     if (interaction.customId === 'premiumbutton'){
-        if (!interaction.member.roles.cache.has(`1179905654345957488`)){
+        if (!interaction.member.roles.cache.has(`1161527100159836170`)){
             return interaction.reply({content: 'Sorry but your NOT a booster :(',ephemeral: true})
             return;
         }
+        interaction.reply({content:'Working on it',ephemeral: true});
+        var user = interaction.user.id;
+        setTimeout(() => {
+            var timenow = interaction.createdTimestamp;
+            var cd = 86000
+            db.get(query, [user], (err, row) => {
+                if (err) {
+                    console.log(err)
+                }
+                if (row === undefined) {
+                    let insertdata = db.prepare(`INSERT INTO data (userid, premium) VALUES(?,?)`);
+                    var timenow = interaction.createdTimestamp;
+                    insertdata.run(user, timenow);
+                    insertdata.finalize();
+                    db.close();
+                    console.log("NEW ROW");
+                    newrow = true;
+                } else if (row.Premium == undefined) {
+                    var timenow = interaction.createdTimestamp;
+                    db.run(`UPDATE data SET Premium = ? WHERE userid = ?;`, [timenow, user]);
+                    console.log("Updating Premium for " + user);
+                    newrow = true;
+                } else {
+                    var time = row.Premium;
+                    var timenow = interaction.createdTimestamp;
+                    total = timenow - time;
+                    console.log("OLD ROW");
+                    console.log(row.Premium)
+                    if (total / 1000 < 86000) {
+                        newrow = false;
+                        console.log(total / 1000);
+                        return;
+
+                    } else {
+                        newrow = true;
+                    };
+                };
+            });
+            var seconds = total / 1000
+            var cooldown = cd - seconds;
+            setTimeout(() => {
+                if (newrow === false) {
+                    interaction.editReply({content: 'Stop Your on cooldown for ' + cooldown + ' seconds',ephemeral: true});
+                    return;
+                }
+            }, 1000);
 
 
-        if(buttonCooldown2.has(interaction.user.id)) {
-            interaction.reply({content: 'Stop Your on cooldown!',ephemeral: true})
-            return;
-        }
 
-        const random2 = Math.floor(Math.random() * premium.length);
-        console.log(random2, premium[random2]);
+            const random2 = Math.floor(Math.random() * premium.length);
+            console.log(random2, premium[random2]);
 
-        const message = premium[random2]
+            const message = premium[random2]
 
-        interaction.reply({content: 'Check your DMS for your premium link',ephemeral: true})
-        
-        const embed3 = new EmbedBuilder()
-        .setTitle("Shady Link Dispenser")
-        .setDescription("Thanks for using Xenapsis")
-        .setColor(0x5900FF)
-        .addFields({name: 'For an extra link boost us.', value: '1 use per 1 week', inline: true})
-        .addFields({name: 'Heres your link! 🔮', value: message, inline: true})
-        .addFields({name: 'Premium links are proven to work', value: 'Free links may be outdated or blocked!'})
-        .addFields({name: 'Do NOT Share 🔨', value: 'Sharing will result in a ban womp'})
-        .setThumbnail('https://xenapsis.github.io/imgs/favicon.png')
+            
+            const embed3 = new EmbedBuilder()
+            .setTitle("Shady Link Dispenser")
+            .setDescription("Thanks for using Xenapsis")
+            .setColor(0x5900FF)
+            .addFields({name: 'For an extra link boost us.', value: '1 use per 1 week', inline: true})
+            .addFields({name: 'Heres your link! 🔮', value: message, inline: true})
+            .addFields({name: 'Premium links are proven to work and updated more often', value: 'Free links may be outdated or blocked!'})
+            .addFields({name: 'Do NOT Share 🔨', value: 'Sharing will result in a ban and the website getting blocked faster by an admin'})
+            .setThumbnail('https://xenapsis.github.io/imgs/favicon.png')
 
-        interaction.member.send({embeds: [embed3]})
+            setTimeout(() => {
+                if (newrow != false) {
+                    interaction.editReply({content: 'Check your DMS for your free link',ephemeral: true})
+                }
+            }, 1010);
 
-        buttonCooldown2.add(interaction.user.id)
-        setTimeout(() => buttonCooldown2.delete(interaction.user.id), 604800_000)
+            setTimeout(() => {
+                if (newrow != false) {
+                    interaction.member.send({embeds: [embed3]});
+                    db.run(`UPDATE data SET Time = ? WHERE userid = ?;`, [timenow, user]);
+                }
+            }, 1010);
 
-    }
+        }, 250);
 
-    if (interaction.customId === 'freebutton'){
-        if(buttonCooldown.has(interaction.user.id)) {
-            interaction.reply({content: 'Stop Your on cooldown!',ephemeral: true})
-            return;
-        }
+    };
+
+    if (interaction.customId === 'freebutton') {
+        setTimeout(() => {
+                var user = interaction.user.id;
+                var timenow = interaction.createdTimestamp;
+                var cd = 86000
+                interaction.reply({content:'Working on it',ephemeral: true});
+                db.get(query, [user], (err, row) => {
+                    if (row === undefined) {
+                        let insertdata = db.prepare(`INSERT INTO data VALUES(?,?,?)`);
+                        var timenow = interaction.createdTimestamp;
+                        insertdata.run(user, timenow, null);
+                        insertdata.finalize();
+                        db.close();
+                        console.log("NEW ROW");
+                        newrow = true;
+                    } else if (row.Time == undefined) {
+                        var timenow = interaction.createdTimestamp;
+                        db.run(`UPDATE data SET Time = ? WHERE userid = ?;`, [timenow, user]);
+                        console.log("Updating Time for " + user);
+                        newrow = true;
+                    } else {
+                        var time = row.Time;
+                        var timenow = interaction.createdTimestamp;
+                        total = timenow - time;
+                        console.log("OLD ROW");
+                        if (total / 1000 < 86000) {
+                            newrow = false;
+                            console.log(total / 1000);
+                            return;
+                        } else {
+                            newrow = true
+                        }
+                    };
+                });
+                var seconds = total / 1000
+                var integer = Math.round(Number(seconds))
+                var cooldown = cd - integer;
+                setTimeout(() => {
+                    if (newrow === false) {
+                        interaction.editReply({content: 'Stop Your on cooldown for ' + cooldown + ' seconds',ephemeral: true});
+                        return;
+                    }
+                }, 1000);
+
+                
 
 
-        interaction.reply({content: 'Check your DMS for your free link',ephemeral: true})
-        const random = Math.floor(Math.random() * links.length);
-        console.log(random, links[random], interaction.user.id);
+                const random = Math.floor(Math.random() * links.length);
+                console.log(random, links[random], interaction.user.id);
 
-        const message = links[random]
+                const message = links[random]
 
-        const embed2 = new EmbedBuilder()
-        .setTitle("Shady Link Dispenser")
-        .setDescription("Thanks for using Xenapsis")
-        .setColor(0x5900FF)
-        .addFields({name: 'For an extra link boost us.', value: '1 use per 1 week', inline: true})
-        .addFields({name: 'Heres your link! 🔮', value: message, inline: true})
-        .addFields({name: 'Premium links are proven to work', value: 'Free links may be outdated or blocked!'})
-        .addFields({name: 'Do NOT Share 🔨', value: 'Sharing will result in a ban womp'})
-        .setThumbnail('https://xenapsis.github.io/imgs/favicon.png')
+                const embed2 = new EmbedBuilder()
+                .setTitle("Shady Link Dispenser")
+                .setDescription("Thanks for using Xenapsis")
+                .setColor(0x5900FF)
+                .addFields({name: 'For an extra link boost us.', value: '1 use per 1 week', inline: true})
+                .addFields({name: 'Heres your link! 🔮', value: message, inline: true})
+                .addFields({name: 'Premium links are proven to work and updated more often', value: 'Free links may be outdated or blocked!'})
+                .addFields({name: 'Do NOT Share 🔨', value: 'Sharing will result in a ban womp'})
+                .setThumbnail('https://xenapsis.github.io/imgs/favicon.png')
+                
+                
+                
+                setTimeout(() => {
+                    if (newrow != false) {
+                        interaction.editReply({content: 'Check your DMS for your free link',ephemeral: true})
+                    }
+                }, 1010);
 
-
-        interaction.member.send({embeds: [embed2]})
-
-        buttonCooldown.add(interaction.user.id)
-        setTimeout(() => buttonCooldown.delete(interaction.user.id), 604800_000)
-    }
+                setTimeout(() => {
+                    if (newrow != false) {
+                        
+                        interaction.member.send({embeds: [embed2]})
+                        db.run(`UPDATE data SET Time = ? WHERE userid = ?;`, [timenow, user]);
+                    }
+                }, 1010);
+        }, 250);
+    };
+            
 });
 
 
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
